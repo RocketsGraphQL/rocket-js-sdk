23cf2128d144779b9fbdcd5defb0164beae8aacf
Kaushik Varanasi <kaushik@Kaushiks-MacBook-Pro.local>
2023-08-02 15:16:24 +0530

    SSR support

--- RocketsAuth.js
+++ RocketsAuth.js
@@ -4,47 +4,19 @@ import Cookies from 'js-cookie';
 
 axios.defaults.withCredentials = false;
 
-function setCookie(name,value,days) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days*24*60*60*1000));
-        expires = "; expires=" + date.toUTCString();
+if (typeof window !== "undefined" && window && window.location.search) {
+    let params = new URLSearchParams(window.location.search);
+    if (params.get('rgraphRefreshToken')) {
+        const refreshToken = params.get('rgraphRefreshToken')
+        console.log("url.searchParams.get('rgraphRefreshToken')", refreshToken);
+        Cookies.set(("refresh", refreshToken, { expires: 365 }));
     }
-    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
-}
-function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    if (params.get('rgraphAccessToken')) {
+        const accessToken = params.get('rgraphAccessToken')
+        console.log("url.searchParams.get('rgraphAccessToken')", accessToken);
+        Cookies.set(("jwt", accessToken, { expires: 7 }));
     }
-    return null;
 }
-function eraseCookie(name) {   
-    document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-}
-
-let params = new URLSearchParams(document.location.search);
-if (params.get('rgraphRefreshToken')) {
-    const refreshToken = params.get('rgraphRefreshToken')
-    console.log("url.searchParams.get('rgraphRefreshToken')", refreshToken);
-    setCookie("refresh", refreshToken, 365);
-}
-if (params.get('rgraphAccessToken')) {
-    const accessToken = params.get('rgraphAccessToken')
-    console.log("url.searchParams.get('rgraphAccessToken')", accessToken);
-    setCookie("jwt", accessToken, 7);
-}
-
-(async () => {
-
-})().catch(err => {
-    console.error(err);
-});
-
 
 export default class Auth {
     constructor(config) {
@@ -53,12 +25,12 @@ export default class Auth {
     }
     async login ({email, password, provider} = {}) {
         if (provider == "local") {
-            const login = await axios.post(this.baseURL+"/signin", {
+            const login = await axios.post(this.baseURL+"/login", {
                 email: email,
                 password: password,
             })
-            const {jwt, refresh} = login.data;
-            Cookies.set("jwt", jwt, { expires: 7 });
+            const {access, refresh} = login.data;
+            Cookies.set("jwt", access, { expires: 7 });
             Cookies.set("refresh", refresh, { expires: 7 });
             return true;
         }
@@ -78,15 +50,30 @@ export default class Auth {
                 Cookies.set("api_url", this.baseURL);
                 return {success: true, redirectUrl: RedirectUrl, providerUrl: ProviderUrl}
             default:
-                const login = await axios.post(this.baseURL+"/signin", {
+                const login = await axios.post(this.baseURL+"/login", {
                     email: email,
                     password: password,
                 })
-                const {jwt, refresh} = login.data;
-                Cookies.set("jwt", jwt, { expires: 7 });
+                const {access, refresh} = login.data;
+                Cookies.set("jwt", access, { expires: 7 });
                 Cookies.set("refresh", refresh, { expires: 7 });
                 return {success: true};
         }
+    }
+    async sendOTP({ phone }) {
+        const otp = await axios.post(this.baseURL + "/sendotp", {
+            Phone: phone,
+        })
+    }
+    async signInWithOTP({ phone, otp }) {
+        const login = await axios.post(this.baseURL + "/signin-with-otp", {
+            Phone: phone,
+            Otp: otp,
+        });
+        const { access, refresh } = login.data;
+        Cookies.set("jwt", access, { expires: 7 });
+        Cookies.set("refresh", refresh, { expires: 7 });
+        return {success: true};
     }
     logout () {
         Cookies.remove("jwt");
@@ -97,8 +84,8 @@ export default class Auth {
             email: email,
             password: password,
         })
-        const {jwt, refresh} = signup.data;
-        Cookies.set("jwt", jwt, { expires: 7 });
+        const {access, refresh} = signup.data;
+        Cookies.set("jwt", access, { expires: 7 });
         Cookies.set("refresh", refresh, { expires: 7 });
         return true;
     }
@@ -115,9 +102,9 @@ export default class Auth {
     }
     async setUser() {
         const cookies = await axios.get(this.baseURL + "/tokens");
-        const { jwt, refreshToken } = cookies.data;
-        Cookies.set("jwt", jwt, { expires: 7 });
-        Cookies.set("refresh", refreshToken, { expires: 7 });
+        const { access, refresh } = cookies.data;
+        Cookies.set("jwt", access, { expires: 7 });
+        Cookies.set("refresh", refresh, { expires: 7 });
     }
     getJWTToken () {
         return Cookies.get("jwt");
